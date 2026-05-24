4a687681e1202f404cc80d111b26b80e43cce4f2
Sohail-Hafeez <sohailhafeez6464@gmail.com>
2026-05-25 00:50:21 +0500

    Conditional Redering

--- Start/src/Components/ListGroup.tsx
+++ Start/src/Components/ListGroup.tsx
@@ -1,8 +1,12 @@
 function ListGroup() {
-    const items = ["pakistan", "india", "bangladesh", "nepal", "srilanka", "bhutan", "maldives", "afghanistan", "myanmar", "thailand", "vietnam", "cambodia", "laos", "malaysia", "indonesia", "singapore", "brunei", "philippines", "east timor", "papua new guinea"];
+    let items = ["pakistan", "india", "bangladesh", "nepal", "srilanka", "bhutan", "maldives", "afghanistan", "myanmar", "thailand", "vietnam", "cambodia", "laos", "malaysia", "indonesia", "singapore", "brunei", "philippines", "east timor", "papua new guinea"];
+
+    items = [];
     return (
         <>
             <h1>List</h1>
+            {items.length === 0 ? <p>No item found</p> : null}
+            {/* {items.length === 0 && <p>No item found</p>} */}
             <ul className="list-group">
                 {items.map((item) => (
                     <li className="list-group-item" key={item}>{item}</li>
