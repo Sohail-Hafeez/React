5544520d4300bf09faa74d5fb9e65abf7bab5d98
Sohail-Hafeez <sohailhafeez6464@gmail.com>
2026-05-25 00:35:24 +0500

    Redering List

--- Start/src/Components/ListGroup.tsx
+++ Start/src/Components/ListGroup.tsx
@@ -1,12 +1,12 @@
 function ListGroup() {
+    const items = ["pakistan", "india", "bangladesh", "nepal", "srilanka", "bhutan", "maldives", "afghanistan", "myanmar", "thailand", "vietnam", "cambodia", "laos", "malaysia", "indonesia", "singapore", "brunei", "philippines", "east timor", "papua new guinea"];
     return (
         <>
             <h1>List</h1>
             <ul className="list-group">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-                <li className="list-group-item">A fourth item</li>
+                {items.map((item) => (
+                    <li className="list-group-item" key={item}>{item}</li>
+                ))}
             </ul>
         </>
     );
